4e04201363c1480e9ffdee8471244da98a9ca3cf
unknown <davidrafaelmsm3@gmail.com>
2021-05-26 20:50:28 -0300

    Adicionando timer de 5 seconds

--- src/main/java/br/com/first/projeto/tela/java/App.java
+++ src/main/java/br/com/first/projeto/tela/java/App.java
@@ -7,6 +7,8 @@ package br.com.first.projeto.tela.java;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 /**
@@ -14,24 +16,30 @@ import org.springframework.jdbc.core.JdbcTemplate;
  * @author Desktop
  */
 public class App {
-    public static void main(String[] args) {
-         Monitoramento mon = new Monitoramento();       
-            Conexao con = new Conexao();
-            JdbcTemplate template = new JdbcTemplate(con.getBanco());
-            
-            Date data = new Date();
-            SimpleDateFormat formatador = new SimpleDateFormat("dd/MM/yyyy"); 
-            String today = formatador.format(data);    
-                  
 
-            
-             System.out.println("Hora de inserir dados");
+    public static void main(String[] args) {
+        int delay = 5000;   // delay de 5 seg.
+        int interval = 5000;  // intervalo de 1 seg.
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
+            public void run() {
+                Monitoramento mon = new Monitoramento();
+                Conexao con = new Conexao();
+                JdbcTemplate template = new JdbcTemplate(con.getBanco());
+
+                Date data = new Date();
+                SimpleDateFormat formatador = new SimpleDateFormat("dd/MM/yyyy");
+                String today = formatador.format(data);
+
+                System.out.println("Hora de inserir dados");
                 System.out.println(mon.getTemperatura());
                 System.out.println(mon.getQtdProcessos().toString());
-             
-            template.update("INSERT INTO dados (ram,temperatura,processador,dataDado,fkUsuario) VALUES (?,?,?,?,?)", 
-                    "1000",mon.getTemperatura(),mon.getQtdProcessos().toString(),today,1);           
-            
-          System.out.println("Dados inseridos");
+
+                template.update("INSERT INTO dados (ram,temperatura,processador,dataDado,fkUsuario) VALUES (?,?,?,?,?)",
+                        "1000", mon.getTemperatura(), mon.getQtdProcessos().toString(), today, 1);
+
+                System.out.println("Dados inseridos");
+            }
+        }, delay, interval);
     }
 }
